18beedf101d08c079a7ea97abe3b20b7498219d9
kaaaka1995 <mdyuan@umich.edu>
2018-11-06 18:12:46 -0500

    add existing heritagesites code to the repo.

---
+++ mysite/secrets.py
@@ -0,0 +1,3 @@
+SECRET_KEY='qymq%g=da$bvnd347ocou1__disf+xx8(3%v5v7&uuuu!d1fko'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY='176291434109-tkms8dmvda4di73rpuneuk11deofitc4.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET='6qow2duHdGJY9H0I4RWAuy6q'
