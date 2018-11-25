aeac5802566de64b570fb8f10ad9a1031d0ce9b6
kaaaka1995 <mdyuan@umich.edu>
2018-11-24 19:56:51 -0500

    ex10

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', lambda r: HttpResponseRedirect('heritagesites/')),
+    path('heritagesites/api/rest-auth/', include('rest_auth.urls')),
+    path('heritagesites/api/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('heritagesites/api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('social_django.urls', namespace='social')),
     path('login/', LoginView.as_view(), name='login'),
